f6f80e08d3cc4cf7c09462b0f2c72f74688c47fb
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-07-19 22:32:23 +0300

    first commit

---
+++ Syntax.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var nameProduct;
+var priceProduct;
+nameProduct = 'Телепорт бытовой VZHIH-101';
+priceProduct = 10000;
+console.log("В наличии имеется: ${nameProduct}");
+console.log('Стоимость товара ${priceProduct} Q');
---
+++ tests.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var a = 5;
+var b = 7;
+console.log(a + b)
